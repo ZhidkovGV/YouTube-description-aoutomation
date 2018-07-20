fd4a111106719e87b44009815b7060a643f07bac
ZhidkovGV <zhidkov.gleb@gmail.com>
2018-07-20 15:37:47 +0300

    WIP

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-(() => {
-    const getDescription = () => {
-
-    }
-})();
+const start = () => {
+        gapi.client.init({
+            'apiKey': 'AIzaSyDcK82q0HarhU9ISfH4hjKRZZ0vwdR0_Aw',
+            // Your API key will be automatically added to the Discovery Document URLs.
+            'discoveryDocs': ['https://people.googleapis.com/$discovery/rest'],
+            // clientId and scope are optional if auth is not required.
+            'clientId': '1089365127935-1ets6q659t9v0g02kged3jirhrc3ideo.apps.googleusercontent.com',
+            'scope': 'profile',
+        }).then(res => res.json()).then(res => console.log(res))
+    };
+gapi.load('client', start);
